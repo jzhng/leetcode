ce16cd9e12a4837b0652eada3ddf1eb2a39520b4
Jin ZHANG <jz.zhangjin@gmail.com>
2022-07-27 22:34:41 +0800

    dp 3

---
+++ 0101-0200/123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<int> dp(5, 0);
+        dp[1] = -prices[0];
+        dp[3] = -prices[0];
+
+        for (int i = 0; i < prices.size(); ++i) {
+            dp[1] = max(dp[1], dp[0] - prices[i]);
+            dp[2] = max(dp[2], dp[1] + prices[i]);
+            dp[3] = max(dp[3], dp[2] - prices[i]);
+            dp[4] = max(dp[4], dp[3] + prices[i]);
+        }
+
+        return dp[4];
+    }
+};
